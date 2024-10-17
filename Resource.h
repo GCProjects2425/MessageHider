ffaf361a2436b17bc82a4ff24e76097dbd9d45dd
GenoWeb <49623340+EvilBrain@users.noreply.github.com>
2024-10-17 15:05:10 +0200

    Gitignore + merge resource

--- Resource.h
+++ Resource.h
@@ -21,6 +21,12 @@
 #define IDM_DECODE_MESSAGE              32778
 #define IDM_SHORTCUT_MENU               32784
 #define IDM_SHOW_LOGS                   32786
+#define ID_VIEW_LIGHTMODE               32787
+#define ID_VIEW_DARKMODE                32788
+#define ID_VIEW_REDMODE                 32789
+#define ID_VIEW_LIMEMODE                32790
+#define ID_VIEW_PURPLEMODE              32791
+#define ID_VIEW_BLUEMODE                32792
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -29,7 +35,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        130
-#define _APS_NEXT_COMMAND_VALUE         32787
+#define _APS_NEXT_COMMAND_VALUE         32793
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
